f6ea3e21b5bd2d8f5f67e7b3a840b48e98509256
pro-masud <promasudbd@gmail.com>
2023-12-23 01:01:46 +0600

    header sub menu icon problem solve successfully complete

--- templates-parts/common/header/navigation.php
+++ templates-parts/common/header/navigation.php
@@ -2,10 +2,20 @@
 
     <h2 class="header__nav-heading h6"><?php echo _e("Site Navigation", "philosophy"); ?></h2>
 
-    <?php wp_nav_menu([
-        'theme_location' => 'top-menu',
-        'menu_class' => 'header__nav',
-    ]);
+    <?php 
+      $headerMenu =   wp_nav_menu([
+            'theme_location' => 'top-menu',
+            'menu_class' => 'header__nav',
+            'menu_id'   => 'header__nav',
+            'echo'  => false,
+        ]);
+
+        $search = 'menu-item-has-children';
+        $replace = 'menu-item-has-children has-children';
+
+        $headerMenu = str_replace($search, $replace, $headerMenu);
+        
+        echo $headerMenu; 
     ?>
 
     <a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu"><?php echo _e("Close", "philosophy"); ?></a>
