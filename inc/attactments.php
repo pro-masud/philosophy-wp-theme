f84556f3ee2e10eec1a87ede80ff91eef810c7aa
pro-masud <promasudbd@gmail.com>
2023-12-23 16:30:19 +0600

    post gallery option warnning successfully complele

--- inc/attactments.php
+++ inc/attactments.php
@@ -5,34 +5,31 @@ add_filter("attachments_default_instance", "__return_false");
 
 function philosophy_attachments($attachments){
     $post_id = null;
+    
+    $post_id = isset($_REQUEST['post']) ? $_REQUEST['post'] : ' ';
 
-    echo $_REQUEST['post_ID'];
-    if($_REQUEST['post']){
-        $post_id = $_REQUEST['post'] ? $_REQUEST['post'] : ' ';
-
-        if(get_post_format($post_id) !="gallery"){
-            return;
-        }
-
-        $fields = [
-            [
-                'name'  => 'Title',
-                'type'  => 'text',
-                'label' => __('Title', 'philosophy')
-            ],
-        ];
-
-        $args = [
-            'label' => __('Gallery Image','philosophy'),
-            'post_type' => ['post'],
-            'filetype'  => ['image'],
-            'note'  => 'Add New Gallery',
-            'button_text'   => __("Gallery Images", "philosophy"),
-            'fields'    => $fields,
-        ];
-
-        $attachments->register('gallery', $args);
+    if(get_post_format($post_id) !="gallery"){
+        return;
     }
+
+    $fields = [
+        [
+            'name'  => 'Title',
+            'type'  => 'text',
+            'label' => __('Title', 'philosophy')
+        ],
+    ];
+
+    $args = [
+        'label' => __('Gallery Image','philosophy'),
+        'post_type' => ['post'],
+        'filetype'  => ['image'],
+        'note'  => 'Add New Gallery',
+        'button_text'   => __("Gallery Images", "philosophy"),
+        'fields'    => $fields,
+    ];
+
+    $attachments->register('gallery', $args);
    
 }
 
